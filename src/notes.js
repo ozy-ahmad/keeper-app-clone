a210018bebd8275d2fce259f3c69dcc4c15388d0
Ahmad Fakhrozy <ahmadfakhrozy@gmail.com>
2020-04-20 00:54:51 +0700

    add notes

---
+++ src/notes.js
@@ -0,0 +1,45 @@
+const notes = [
+  {
+    id: 1,
+    title: "Delegation",
+    content:
+      "Q. How many programmers does it take to change a light bulb? A. None – It’s a hardware problem",
+  },
+  {
+    id: 2,
+    title: "Loops",
+    content:
+      "How to keep a programmer in the shower forever. Show him the shampoo bottle instructions: Lather. Rinse. Repeat.",
+  },
+  {
+    id: 3,
+    title: "Arrays",
+    content:
+      "Q. Why did the programmer quit his job? A. Because he didn't get arrays.",
+  },
+  {
+    id: 4,
+    title: "Hardware vs. Software",
+    content:
+      "What's the difference between hardware and software? You can hit your hardware with a hammer, but you can only curse at your software.",
+  },
+  {
+    id: 5,
+    title: "Big ideas",
+    content: "Eat more sushi",
+  },
+  {
+    id: 6,
+    title: "Lorem Ipsum",
+    content:
+      "Enim voluptate et aliquip anim ea proident ut quis magna culpa Lorem sunt. Labore velit ad qui dolor eiusmod nisi eiusmod irure velit dolor sit amet. ",
+  },
+  {
+    id: 7,
+    title: "Lorem Ipsum Lagi",
+    content:
+      "Cillum ad nisi cillum anim esse id dolore ipsum exercitation eu nostrud laborum aute. Nostrud deserunt tempor tempor labore laboris officia deserunt do ut non. Magna cillum laborum eu tempor et dolore ipsum irure eiusmod. ",
+  },
+];
+
+export default notes;
